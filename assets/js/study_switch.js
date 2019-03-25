97b805cabaf873e7c030a21b4f501be68244ad76
Briana Douglas <briana.douglas@icloud.com>
2019-03-25 15:26:30 -0400

    Hides description when anywhere else is clicked:

--- assets/js/study_switch.js
+++ assets/js/study_switch.js
@@ -11,9 +11,20 @@ function show_tab(tab_name) {
 }
 
 
-s1_button = document.getElementById('study1_box')
-s2_button = document.getElementById('study2_box')
+s1_button = document.getElementById('study1_box');
+s2_button = document.getElementById('study2_box');
 
 
 s1_button.onclick = function() {show_tab('study_1_description')};
 s2_button.onclick = function () {show_tab('study_2_description')};
+
+tabs = ['study1_box', 'study2_box'];
+
+//remove the descriptions when anywhere else is clicked
+$(document).on('click', function (e) {
+	find = tabs.indexOf(e.target.id);
+	if (find === -1) {
+		document.getElementById('study_1_description').style.display = "none";
+		document.getElementById('study_2_description').style.display = "none";
+	} 
+});
